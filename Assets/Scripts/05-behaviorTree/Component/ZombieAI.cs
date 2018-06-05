81667ed80c00e6d94b7c7e9a9bda1134276e2159
Icelanguage <845854051@qq.com>
2018-06-06 00:21:17 +0800

    prepare for behaviorTree scene

---
+++ Assets/Scripts/05-behaviorTree/Component/ZombieAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinHowBehaviorTree
+{
+    /// <summary>
+    /// 僵尸AI
+    /// </summary>
+    public class ZombieAI : MonoBehaviour
+    {
+        private BehaviorTreeAction behaviorTree = new BehaviorTreeAction();
+        private void Start()
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/Common/BehaviorTree/BehaviorTreeAction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class BehaviorTreeAction:BehaviorTreeNode
+{
+    protected static int uniqueKey = 0;
+    /// <summary>
+    /// 获取唯一的key值(每次调用,key加一)
+    /// </summary>
+    public static int GetUniqueKey()
+    {
+        if (uniqueKey >= int.MaxValue) return uniqueKey = 0;
+        return ++uniqueKey;
+
+    }
+    public string Name { get; set; }
+
+    protected BehaviorTreePrecondition precondition;
+
+    public BehaviorTreeAction()
+    {
+        uniqueKey = BehaviorTreeAction.GetUniqueKey();
+    }
+
+    public override int GetHashCode()
+    {
+        return uniqueKey;
+    }
+
+    public bool Evaluate()
+    {
+        return (precondition == null || precondition.IsTrue()) && onEvaluate();
+    }
+    public void Transition()
+    {
+        onTransition();
+    }
+    public BehaviorTreeAction SetPrecondition(BehaviorTreePrecondition precondition)
+    {
+        this.precondition = precondition;
+        return this;
+    }
+
+    //--------------------------------------------------------
+    // inherented by children
+    protected virtual bool onEvaluate()
+    {
+        return true;
+    }
+    protected virtual void onTransition()
+    {
+    }
+}
