632cbee09d744841a70f2afbf6ea1fd5a62c3834
Icelanguage <845854051@qq.com>
2018-06-16 20:40:35 +0800

    SightSensor and SightTraigger finish

---
+++ Assets/Scripts/08-PerceptualSystem/Component/SoundTraigger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace LinhowePerceptualSystem
+{
+    /// <summary>
+    /// 听觉触发器
+    /// </summary>
+    public class SoundTraigger:LimitedTimeTraigger
+    {
+        public float radius;
+        protected override void Awake()
+        {
+            PerceptualTyep = PerceptualEnum.Sound;
+            duration = 3;
+            base.Awake();
+
+        }
+
+        protected override bool IsTraigger(BaseSensor sensor)
+        {
+            if(Vector3.Distance(sensor.transform.position,transform.position)
+                <radius)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+}
