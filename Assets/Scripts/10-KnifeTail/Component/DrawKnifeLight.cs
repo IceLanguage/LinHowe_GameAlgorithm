fabb7ec8fb6f4e2d8939b3180503574fc46ddaa4
Icelanguage <845854051@qq.com>
2018-06-18 01:28:37 +0800

    Test GL DrawLine

---
+++ Assets/Scripts/10-KnifeTail/Component/DrawKnifeLight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinHoweKnifeTail
+{
+    public class DrawKnifeLight : MonoBehaviour
+    {
+        //public Material mat;
+        public Transform KnifeTip;
+
+        private Queue<Vector3> Points = new Queue<Vector3>();
+
+        private void Update()
+        {
+            Vector2 v = Camera.main.WorldToScreenPoint(KnifeTip.position);
+            Points.Enqueue(new Vector3(v.x, v.y, 0));
+        }
+        
+        void OnPostRender()
+        {
+            //if (!mat)
+            //{
+            //    Debug.LogError("Please Assign a material on the inspector");
+            //    return;
+            //}
+            //GL.PushMatrix(); //保存当前Matirx  
+            //mat.SetPass(0); //刷新当前材质  
+            GL.LoadPixelMatrix();//设置pixelMatrix  
+            //GL.Color(Color.black);
+            int count = Points.Count;
+            var list = Points.ToArray();
+            for(int i = 0;i < count - 1;++i)
+            {
+                GL.Begin(GL.LINES);
+                GL.Vertex(list[i]);
+                GL.Vertex(list[i + 1]);
+                GL.End();
+            }
+            if(Points.Count>300)
+                Points.Dequeue();
+            //GL.Begin(GL.LINES);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(v.x, v.y, 0);
+            //GL.End();
+            //GL.PopMatrix();//读取之前的Matrix  
+        }
+    }
+}
---
+++ Assets/Scripts/10-KnifeTail/Component/SwordController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LinHoweKnifeTail
+{
+    public class SwordController : MonoBehaviour
+    {
+
+        private float timer = 0;
+        public float RotateInterval = 0.1f;
+
+        private void FixedUpdate()
+        {
+            timer += Time.fixedDeltaTime;
+
+            if (timer > RotateInterval)
+            {
+                int RandomIndex = Random.Range(0, 3);
+                if (RandomIndex < 1)
+                    transform.rotation = Quaternion.AngleAxis(Random.Range(-90, 90), transform.right) * transform.rotation;
+                else if (RandomIndex < 2)
+                    transform.rotation = Quaternion.AngleAxis(Random.Range(-90, 90), transform.up) * transform.rotation;
+                else
+                    transform.rotation = Quaternion.AngleAxis(Random.Range(-90, 90), transform.forward) * transform.rotation;
+                timer = 0;
+            }
+        }
+
+        
+        
+    }
+}
+
