571fbaab9eca2ba2ae084bbd2d5b9e86735d7bfe
Icelanguage <845854051@qq.com>
2018-06-18 14:23:34 +0800

    finish BezierCurve

---
+++ Assets/Scripts/10-KnifeTail/Algorithm/BezierCurve.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace LinHoweKnifeTail
+{
+    /// <summary>
+    /// 贝塞尔曲线
+    /// </summary>
+    public class BezierCurve
+    {
+        //计算后曲线
+        public List<Vector3> Curve { get; set; }
+        private Vector3[] points;
+        private Dictionary<int, float> helpDic = new Dictionary<int, float>();
+        private int PointNumber;
+        public BezierCurve(Vector3[] points,int PointNumber)
+        {
+           
+            this.points = points;
+            
+            this.PointNumber = PointNumber;
+            Curve = new List<Vector3>();
+
+            float s = 1 / (float)PointNumber;
+            for(int i = 0;i <= PointNumber; ++i)
+            {
+                float t = s * i;
+                Curve.Add(PointOnCubicBezier(t));
+
+            }
+        }
+
+        private Vector3 PointOnCubicBezier(float t)
+        {
+            //计算多项式系数
+            Vector3 res = Vector3.zero;
+            for(int i = 0;i<points.Length;++i)
+            {
+                float p = help(i);
+                float q = Mathf.Pow(t, i);
+                float m = Mathf.Pow(1 - t, points.Length - i - 1);
+                res += p * points[i] * q * m;
+            }
+            
+            if(res.x<10)
+            {
+                Debug.Log(t);
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 计算多项式系数
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private float help(int i)
+        {
+            
+            float res = 1;
+            if (helpDic.ContainsKey(i))
+                return helpDic[i];
+            //if (helpDic.TryGetValue(i, out res))
+            //    return res;
+            for (int j = 0;j < i;++j)
+            {
+                res *= points.Length - 1 - j ;
+                res /= j + 1;
+            }
+            helpDic.Add(i, res);
+            return res;
+        }
+
+    }
+}
--- Assets/Scripts/10-KnifeTail/Component/DrawKnifeLight.cs
+++ Assets/Scripts/10-KnifeTail/Component/DrawKnifeLight.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace LinHoweKnifeTail
@@ -14,7 +15,11 @@ namespace LinHoweKnifeTail
         private void Update()
         {
             Vector2 v = Camera.main.WorldToScreenPoint(KnifeTip.position);
-            Points.Enqueue(new Vector3(v.x, v.y, 0));
+            Vector3 newv = new Vector3(v.x, v.y, 0);
+            if(0 == Points.Count || newv != Points.Last())
+                Points.Enqueue(newv);
+            
+
         }
         
         void OnPostRender()
@@ -27,22 +32,23 @@ namespace LinHoweKnifeTail
             //GL.PushMatrix(); //保存当前Matirx  
             //mat.SetPass(0); //刷新当前材质  
             GL.LoadPixelMatrix();//设置pixelMatrix  
-            //GL.Color(Color.black);
+            GL.Color(Color.black);
             int count = Points.Count;
-            var list = Points.ToArray();
-            for(int i = 0;i < count - 1;++i)
+           
+            while(Points.Count>3)
+            {
+                Points.Dequeue();
+            }
+            var bezierCurve =  new BezierCurve(Points.ToArray(), 100);
+            for (int i = 0; i < bezierCurve.Curve.Count - 1; ++i)
             {
                 GL.Begin(GL.LINES);
-                GL.Vertex(list[i]);
-                GL.Vertex(list[i + 1]);
+                GL.Vertex(bezierCurve.Curve[i]);
+                GL.Vertex(bezierCurve.Curve[i + 1]);
                 GL.End();
             }
-            if(Points.Count>300)
-                Points.Dequeue();
-            //GL.Begin(GL.LINES);
-            //GL.Vertex3(0, 0, 0);
-            //GL.Vertex3(v.x, v.y, 0);
-            //GL.End();
+
+            
             //GL.PopMatrix();//读取之前的Matrix  
         }
     }
